b7d2b7d3dd353c7c9908c0238d1d27dc13595fb7
artificial-life <proteus.protocol@gmail.com>
2015-08-08 13:58:11 +0600

    rework init

--- src/Support/volume.js
+++
@@ -1,9 +0,0 @@
-'use strict'
-
-class Volume {
-    constructor(definition) {
-
-    }
-}
-
-module.exports = Volume;
--- src/workflow.js
+++
@@ -1,8 +0,0 @@
-'use strict'
-
-class Workflow {
-    constructor(Schema) {
-        console.log('ворк машт флоу...');
-    }
-
-}
